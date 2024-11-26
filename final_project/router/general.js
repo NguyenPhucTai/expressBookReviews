13ee0a4241c20d8dc44473b533bb6688322bf0ff
Nguyễn Phúc Tài <62977249+NguyenPhucTai@users.noreply.github.com>
2024-11-26 14:43:34 +0700

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,17 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+  // const { username, password } = req.body;
+  // if(!username || !password){
+  //   return res.json({ message: "Username or password is missing!" });
+  // }
+
+  // const existUsername = users[username];
+  // if(!existUsername){
+  //   return res.json({ message: "Username is not existed!" });
+  // }
   return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get the book list available in the shop
@@ -17,54 +26,58 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
+  const { isbn } = req.params;
   const book = books[isbn];
+
   if(book){
     return res.json(book);
   }
+
   return res.json({ message: `Not found book with isbn ${isbn}` });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  const booksByAuthor = [];
 
-  for(let key in books){
-    if(books[key].author.toUpperCase() === author.toUpperCase()){
-      booksByAuthor.push(books[key]);
-    }
-  }
+  const { author } = req.params;
+  const booksByAuthor = Object.values(books).filter(book => book.author.toUpperCase() === author.toUpperCase());
 
   if(booksByAuthor.length > 0){
     return res.json(booksByAuthor);
-  } else{
-    return res.json({ message: `No books found for author ${author}` });
   }
+
+  return res.json({ message: `No books found for author ${author}` });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  const booksByTitle = [];
+  const { title } = req.params;
 
-  for(let key in books){
-    if(books[key].title.toUpperCase() === title.toUpperCase()){
-      booksByTitle.push(books[key]);
-    }
-  }
+  const booksByTitle = Object.values(books).filter(book => book.title.toUpperCase() === title.toUpperCase());
 
-  if(booksByTitle.length > 0){
+  if (booksByTitle.length > 0) {
     return res.json(booksByTitle);
-  } else{
-    return res.json({ message: `No books found for title ${title}` });
   }
+
+  return res.json({ message: `No books found for title ${title}` });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const book = books[isbn];
+
+  if(!book){
+    return res.json({ message: `Not found book with isbn ${isbn}` });
+  }
+
+  const { reviews, title } = book;
+
+  if (Object.keys(reviews).length === 0) {
+    return res.json({ message: `No review for book ${title}` });
+  }
+
+  return res.json(reviews);
 });
 
 module.exports.general = public_users;
