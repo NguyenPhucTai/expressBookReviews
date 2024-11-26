f4f879d45f13751fcef3fdceb37fa893f628f140
Nguyễn Phúc Tài <62977249+NguyenPhucTai@users.noreply.github.com>
2024-11-26 15:29:47 +0700

    Task 7 completed

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,33 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  return users.some(user => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  const user = users.find(user => user.username === username);
+  return user && user.password === password;
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!isValid(username)) {
+    return res.status(400).json({ message: "Username does not exist." });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(400).json({ message: "Invalid username or password." });
+  }
+
+  const token = jwt.sign({username : username}, 'tai_phuc_key', { expiresIn: '1h' });
+
+  return res.status(200).json({
+    message: "Login successful",
+    token: token,
+  });
 });
 
 // Add a book review
